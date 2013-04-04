22956805951b4fa33c1ef0ec78027e7054af5577
Ramamohan Suryadevara <srmmsu@gmail.com>
2013-04-04 19:37:45 +0530

    Added comments with API reference

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,7 @@
+// Please note, here 'console' a predefined global object
+// 'log' is a method available in 'console'.
+// See more at http://nodejs.org/api/stdio.html#stdio_console_log_data 
+
 console.log("Hello World !");
+
 
