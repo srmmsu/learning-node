1d579835eff31025bf5c262cf60388a62abfa043
Ramamohan Suryadevara <srmmsu@gmail.com>
2013-04-11 19:59:07 +0530

    Added mathjs library with a small app

---
+++ mathjs-examples.js
@@ -0,0 +1,5 @@
+var mathlib = require('mathjs');
+var sqrt2 = mathlib.sqrt(2.0);
+console.log(" square root of 2 = "+sqrt2);
+var sqrt_2 = mathlib.sqrt(-2.0);
+console.log(" square root of -2 = "+sqrt_2);
